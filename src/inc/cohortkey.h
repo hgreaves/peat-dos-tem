4092cb423c646836f53eb42a781bcff736db0615
Heather E Greaves <hegreaves@alaska.edu>
2018-02-27 14:20:52 -0900

    Add parameters for boreal bog

--- src/inc/cohortkey.h
+++ src/inc/cohortkey.h
@@ -1,6 +1,6 @@
 #ifndef COHORTKEY_H_
 	#define COHORTKEY_H_
-  	enum VEGKEY{I_TUNDRA, I_DF, I_WSF, I_BSF};
+  	enum VEGKEY{I_TUNDRA, I_DF, I_WSF, I_BSF, I_BB};
   	enum DRGKEY{I_DRY, I_WET};
   
 #endif /*COHORTKEY_H_*/
